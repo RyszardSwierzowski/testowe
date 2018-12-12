5c6e9f4a1c8efe2b90640107a9484f6cde0d5096
marczin <marcinrosol1997@gmail.com>
2018-12-12 19:42:21 +0100

    chsnge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,18 +6,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public Main main;
+   
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("viewsFxml/rejestracja.fxml"));
         primaryStage.setTitle("Twój Trening");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
-        main = this;
+
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
+
     }
 }
