477f10c3bbdf6d25b34c71055457853d9c6dc80b
marczin <marcinrosol1997@gmail.com>
2018-12-12 20:05:06 +0100

    changes

--- src/main/java/controllers/Rejestracja.java
+++ src/main/java/controllers/Rejestracja.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -34,12 +35,21 @@ public class Rejestracja {
     private TextField imieField;
 
     /* ----------- */
+    ValidateUtilities validate = new ValidateUtilities();
 
 
     @FXML
     public void onActionZarejestrujButton(){
+        System.out.println("klikniecie");
+        try{
+            if(validate.compareRegisterPasswd(hasloField, powtorzhasloField)){
+                System.out.println("hasło takie same");
 
-        ValidateUtilities validate = new ValidateUtilities();
+            }
+
+        }catch (Exception ex){
+
+        }
 
     }
 
--- src/main/java/utilities/ValidateUtilities.java
+++ src/main/java/utilities/ValidateUtilities.java
@@ -1,17 +1,28 @@
 package utilities;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class ValidateUtilities {
 
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+
     public ValidateUtilities() {
     }
 
     public boolean compareRegisterPasswd(PasswordField first, PasswordField repeat){
-    if(first.equals(repeat)) return true;
-    else return false;
+        if(!(first.equals(null) || repeat.equals(null))){
+            if(first.equals(repeat)) return true;
+            else return false;
+        }else{
+            return false;
+        }
     }
 
+    public boolean validateEmail(TextField email){
+
+        return false;
+    }
 
 }
