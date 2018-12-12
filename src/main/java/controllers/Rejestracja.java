6b5d50153ac7246517e228a292ca221346232877
marczin <marcinrosol1997@gmail.com>
2018-12-12 22:54:10 +0100

    Wprowadzenie zmian w rejestracji
    wprowadzenie funkcjonalnosci walidacji danych oraz implementacja jej podczas rejestracji.

--- src/main/java/controllers/Rejestracja.java
+++ src/main/java/controllers/Rejestracja.java
@@ -41,14 +41,27 @@ public class Rejestracja {
     @FXML
     public void onActionZarejestrujButton(){
         System.out.println("klikniecie");
-        try{
-            if(validate.compareRegisterPasswd(hasloField, powtorzhasloField)){
-                System.out.println("hasło takie same");
+        try{ //TODO: poprawic to bo wyglada marnie
+            if(validate.compareRegisterPasswd(hasloField.getText(), powtorzhasloField.getText())){
+                if (validate.validateTextField(imieField.getText())){ //imie
+                    if(validate.validateTextField(nazwiskoField.getText())){ //nazwisko
+                        if(validate.validateTextField(loginField.getText())){ //login
+                            if(validate.validateNumberField(telefonField.getText())){//telefon
+                                if(validate.validateEmail(emailField.getText())){//email
 
+                                    System.out.println("UDALO SIE PREJSC WERIFIKACJE JEEEEJ");
+                                    //todo: stworzyc obiekt uzytkownika w sumie xD
+                                    //TODO: w przyszłości dodac tutaj jakies polacznie z baza danych i wyslac polecenie dodania uzytkownika
+                                    //todo: aha no i jak kliknie to tam przeneiesc do jakiegos nwm strony logowania albo wyswietlic glowna strone aplikacji
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
         }catch (Exception ex){
-
+            System.out.println("BUUUU");
         }
 
     }
--- src/main/java/utilities/ValidateUtilities.java
+++ src/main/java/utilities/ValidateUtilities.java
@@ -11,18 +11,72 @@ public class ValidateUtilities {
     public ValidateUtilities() {
     }
 
-    public boolean compareRegisterPasswd(PasswordField first, PasswordField repeat){
-        if(!(first.equals(null) || repeat.equals(null))){
-            if(first.equals(repeat)) return true;
-            else return false;
+    public boolean compareRegisterPasswd(String first, String repeat){
+        System.out.println("walidacja");
+
+        alert.setTitle("Błąd rejestracji");
+        alert.setHeaderText(null);
+
+        if(!(first.equals("") || repeat.equals(""))){ //sprawdzam czy hasla nie sa puste
+            System.out.println("validate not null");
+
+            if(!first.contains(" ")){ //sprawdzenie czy zawiera znaki ktoych nie chcemy
+
+                if(first.equals(repeat)){ //sprawdzenie czy sa takie same
+                    return true;
+                }
+                else{
+                    return false;
+                }
+
+            }else{
+                alert.setContentText("podane hasło zawiera niedozwolone znaki");
+                alert.showAndWait();  //--
+                return false;
+            }
+
+
         }else{
+
+            alert.setContentText("podane hasło jest puste");
+            alert.showAndWait();
             return false;
         }
     }
 
-    public boolean validateEmail(TextField email){
+    public boolean validateEmail(String email){
+    boolean result = true;
 
-        return false;
+    if(!email.contains("@")) result = false; //prosta walidacja czy nie zawira @
+    return result;
     }
+
+    public boolean validateTextField(String string){ //funkcja walidujaca zwykly ciag znakowy czy zawiera niedozwolone znaki
+        boolean result = true;
+
+        if(string.contains(" ")) result = false;
+        char[] Array=string.toCharArray();
+        for (char x:Array)
+        {
+            if( !( (x<=95&&x>=65) || (x<=122&&x>=97) || (x<=57&&x>=48) ) )
+                result = false;
+        }
+        return result;
+    }
+
+    public boolean validateNumberField(String str){
+        boolean result = true;
+
+        if(str.contains(" ")) result = false;
+        char[] Array = str.toCharArray();
+        for(char x:Array){
+            if(!(x>=48 && x<=57))
+                result = false;
+        } //48 - > 57 ascii dla liczb
+
+        return result;
+    }
+
+
 
 }
