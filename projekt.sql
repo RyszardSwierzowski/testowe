3f34ed0e75fd43fd2ffddea7809938e8185e522f
RyszardSwierzowski <36984890+RyszardSwierzowski@users.noreply.github.com>
2019-01-16 21:04:55 +0100

    Update projekt.sql

--- projekt.sql
+++ projekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 16 Sty 2019, 18:08
+-- Czas generowania: 16 Sty 2019, 21:04
 -- Wersja serwera: 10.1.37-MariaDB
 -- Wersja PHP: 7.3.0
 
@@ -69,7 +69,9 @@ INSERT INTO `logindata` (`login`, `password`, `idUser`) VALUES
 ('dasdassda', '123456789', '7'),
 ('dasdassda', 'dasdassda', '8'),
 ('dasdassda', 'ewqeqweaa', '9'),
-('wqerptl1', 'Qwerty123!', '10');
+('wqerptl1', 'Qwerty123!', '10'),
+('qw123', 'Qwerty123!', '11'),
+('qwe123', 'Qwerty123!', '12');
 
 -- --------------------------------------------------------
 
@@ -89,7 +91,8 @@ CREATE TABLE `platnosci` (
 --
 
 INSERT INTO `platnosci` (`idUser`, `status`, `karta`, `termin`) VALUES
-(4, 'NIE_DODANO_KARTY', 0, '-');
+(4, 'NIE_DODANO_KARTY', 0, '-'),
+(13, 'NIE_DODANO_KARTY', 0, '-');
 
 -- --------------------------------------------------------
 
@@ -181,13 +184,15 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`IDuser`, `login`, `imie`, `nazwisko`, `email`, `telefon`) VALUES
 (1, 'JN54', 'Jacek', 'Nowak', 'Nowak@cos.com', '123456789'),
 (3, 'jacus', 'Jacu?', 'Kolo', 'ewqeqw@wp.pl', '123456987'),
-(4, 'admin', 'Krzysztof', 'Katek', 'eqeeq@wp.pl', '793695369'),
+(4, 'admin', 'Admin', 'Admin', 'admin@admin.pl', '777777777'),
 (5, 'ewqeqwe', 'LDADA', 'dsad', 'DAsda@ww', '123456789'),
 (6, 'dasdassda', 'eqwew', 'eqweqw', 'eqweq@wp.pl', '123456789'),
 (7, 'dasdassda', 'dasdas', 'dasda', 'dasdadasdasdas', '123456789'),
 (8, 'dasdassda', 'dasdassda', 'dasdassda', 'dasdassda', '123456789'),
 (9, 'dasdassda', 'ewqeqwe', 'ewqeqwe', '12345678978', '123456789'),
-(10, 'wqerptl1', 'tomasz', 'nowak', 'qwer@wp.pl', '123456789');
+(10, 'wqerptl1', 'tomasz', 'nowak', 'qwer@wp.pl', '123456789'),
+(11, 'qw123', 'Roman', 'Pazdzioch', 'qeqweqw@wp.pl', '789456987'),
+(12, 'qwe123', 'eqweqwe', 'eqweq', 'eqweqe@wp.pl', '123456789');
 
 -- --------------------------------------------------------
 
@@ -199,6 +204,13 @@ CREATE TABLE `zapisy` (
   `idUser` int(11) NOT NULL,
   `idTerminu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `zapisy`
+--
+
+INSERT INTO `zapisy` (`idUser`, `idTerminu`) VALUES
+(12, 2);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
